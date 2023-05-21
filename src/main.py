fae856eb7dad90eb1ba968fcd15565c983382c9b
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-05-21 23:25:17 +0530

    Telegram bot files added.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,9 @@ TOKEN = os.getenv('TOKEN')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot_text = "I'm a bot, Created by Arabian Coconut\nMade to do Biological computations"
+    user = update.message.from_user.full_name
     await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Welcome {user}!")
 
 
 async def sequencer(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -34,7 +36,8 @@ def main():
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler('sequencer', sequencer))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, sequence_handle))
-    app.run_polling(3)
+    app.run_polling(5)
+
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
